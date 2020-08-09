9c8e1ea9e216f14869a842adacfbbd3d63ba2a9b
Staneesh <blkreasons@outlook.com>
2020-08-09 13:23:39 +0200

    ✨ Dice generating function debug

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+fn generate_dice(how_many: u32) -> Vec<u32> {
+    let mut res = Vec::<u32>::new();
+
+    for i in 0..how_many {
+        res.push(i);
+    }
+
+    return res;
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("intro...");
+
+    let dice = generate_dice(2);
+    println!("Dice: {:?}", dice);
 }
